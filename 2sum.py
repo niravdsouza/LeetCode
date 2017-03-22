d50e077f95525ecdb0c7e9ec7a2ba5f47e790255
Nirav <niravdsouza@gmail.com>
2017-03-22 00:53:19 -0400

    Update 2sum.py
    Updated

--- 2sum.py
+++ 2sum.py
@@ -10,3 +10,18 @@ Given nums = [2, 7, 11, 15], target = 9,
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
 """
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        test={}
+        for i,num in enumerate(nums):
+            if num in test:
+                num1=i
+                num2=test[num]
+            test[target-num]=i
+        
+        return [num2,num1]
